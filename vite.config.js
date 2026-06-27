11ac64d3222949186d14b9953e57a70eb2382946
umer250 <umerm8809@gmail.com>
2026-06-27 22:17:02 +0500

    build issue

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          framerMotion: ['framer-motion'],
+          icons: ['react-icons']
+        }
+      }
+    }
+  },
+  base: './'
 })
